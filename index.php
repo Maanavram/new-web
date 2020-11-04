446d1090c076347ccb059a3c26fdb293d403464b
Maanavram <73928414+Maanavram@users.noreply.github.com>
2020-11-04 19:39:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php include ('header.php'); ?>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="assets/air img2.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="assets/airplane.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="assets/air img3.jpg" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <div class="container">
+  <h2>SPECIALIZATION</h2><br>
+  <br><hr>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="thumbnail">
+        <a href="assets/amro.html" target="_blank">
+          <img src="assets/img2.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="thumbnail">
+        <a href="assets/maintanance.html" target="_blank">
+          <img src="assets/img3.jpg" alt="Nature" style="width:100%">
+          <div class="caption">
+            
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="thumbnail">
+        <a href="assets/amro.html" target="_blank">
+          <img src="assets/img4.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            
+          </div>
+        </a>
+      </div>
+    </div>
+    
+            
+          
+    
+    <div class="col-md-3">
+      <div class="thumbnail">
+        <a href="#" target="_blank">
+         <p>India’s leading diversified aviation services provider is the preferred MRO partner for OEMs, aircraft owners, lessors, airlines and Defence Services
+Established in 1951, Air Works is a CAR 145 organization and it is today, India’s largest independent MRO provider with a presence in 27 cities.</p>
+          <div class="caption">
+            
+          </div>
+        </a>
+      </div>
+    </div>
+    
+</div>
+
+
+<?php include ('footer.php'); ?>
